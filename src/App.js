a82be131dfdffaa7c438f78967e09893752eaece
sabdahtb <sabdahutabarat2804@gmail.com>
2022-03-12 15:52:17 +0700

    install dan test react, mui serta mui/styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from "@mui/material";
+import { yellow } from "@mui/material/colors";
+import { makeStyles } from "@mui/styles";
+import React from "react";
 
-function App() {
+const useStyle = makeStyles({
+  testo: {
+    marginTop: 20,
+    marginLeft: 30,
+    backgroundColor: yellow[400],
+  },
+});
+
+export default function App() {
+  const classes = useStyle();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography variant="h1" color="secondary" className={classes.testo}>
+        Halooo MAsseehhh
+      </Typography>
     </div>
   );
 }
-
-export default App;
